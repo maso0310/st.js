dea78c1dedf4f49c838cdee63d345855206f9b4d
unknown <balimon@hotmail.com>
2018-02-08 23:51:04 +0800

    'ok'

--- index備份.js
+++ index備份.js
@@ -9,7 +9,7 @@ var bot = linebot({
 
 //這邊想要做的是判讀如何回應
 bot.on('message', function(event) {
-  if (event.message.type = 'text') {
+  if (event.message.type = "我想要，"+'text') {
     var msg = "https://tw.shop.com/search/"+event.message.text;
   //收到文字訊息時，直接把收到的訊息傳回去
     event.reply(msg).then(function(data) {
