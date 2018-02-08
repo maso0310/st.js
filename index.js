fd17cc6091508dff0e5f7e6b3ad496f6837fb57c
unknown <balimon@hotmail.com>
2018-02-08 23:56:51 +0800

    'ok'

--- index.js
+++ index.js
@@ -7,14 +7,20 @@ var bot = linebot({
   channelAccessToken: '295PO7c2FbLmq2jHznxmISTQciVcWy7DnEIArWCgSUKWPsorlKbbuRURYIYxWZYJXjlQ2Wj9Ik8nwuAz2qB2rc4EfKN56kdvBERkjx7F1OOPsjx8cdCNGhoLQtVfAR+TrFsuw73JsEKI2IA9gE719gdB04t89/1O/w1cDnyilFU='
 });
 
-//這邊想要做的是如何讀取資料開放平台的資料
+//這邊想要做的是判讀如何回應
 bot.on('message', function(event) {
-var request = require('request); 
- request('http://www.google.com' , function (error , response, body){
-   console.log('error:',error);
-   console.log('statuCode:' , response && response.statusCode);
-   console.log('body:', body);
-}}
+  if (event.message.type = "我想要，"+'text') {
+    var msg = "https://tw.shop.com/search/"+event.message.text;
+  //收到文字訊息時，直接把收到的訊息傳回去
+    event.reply(msg).then(function(data) {
+      // 傳送訊息成功時，可在此寫程式碼 
+      console.log(msg);
+    }).catch(function(error) {
+      // 傳送訊息失敗時，可在此寫程式碼 
+      console.log('錯誤產生，錯誤碼：'+error);
+    });
+  }
+});
 
 const app = express();
 const linebotParser = bot.parser();
