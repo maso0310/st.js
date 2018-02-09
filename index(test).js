18e6ec4787916bbf7c80a274b9304765b8c08913
unknown <balimon@hotmail.com>
2018-02-10 03:04:39 +0800

    'ok'

--- index(test).js
+++ index(test).js
@@ -12,13 +12,18 @@ var bot = linebot({
 //這邊想要做的是判讀如何回應
 bot.on('message', function(event) {
   if (event.message.type = 'text') {
-    var msg = "https://tw.shop.com/search/"+event.message.text;
+    var request = require("request");
+    var cheerio = require("cheerio");
+    const url = 'https://tw.shop.com/maso0310/search/'+event.message.text;
+    request(url, (err, res, body) => {
+      console.log(body)
+    })
   //收到文字訊息時，直接把收到的訊息傳回去
-    event.reply(msg).then(function(data) {
-      // 傳送訊息成功時，可在此寫程式碼 
-      console.log(msg);
+    event.reply(request).then(function(data) {
+      // 傳送訊息成功時，可在此寫程式碼
+      console.log(request);
     }).catch(function(error) {
-      // 傳送訊息失敗時，可在此寫程式碼 
+      // 傳送訊息失敗時，可在此寫程式碼
       console.log('錯誤產生，錯誤碼：'+error);
     });
   }
