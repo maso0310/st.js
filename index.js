76c03bddfcd8337791e56d8ca02a883b2edaa70b
unknown <balimon@hotmail.com>
2018-02-10 00:52:39 +0800

    'ok'

--- index.js
+++ index.js
@@ -1,47 +1,34 @@
-const line = require('@line/bot-sdk');
-const express = require('express');
+var linebot = require('linebot');
+var express = require('express');
 
-// create LINE SDK config from env variables
-const config = {
-  channelAccessToken: process.env.'295PO7c2FbLmq2jHznxmISTQciVcWy7DnEIArWCgSUKWPsorlKbbuRURYIYxWZYJXjlQ2Wj9Ik8nwuAz2qB2rc4EfKN56kdvBERkjx7F1OOPsjx8cdCNGhoLQtVfAR+TrFsuw73JsEKI2IA9gE719gdB04t89/1O/w1cDnyilFU=',
-  channelSecret: process.env.'6baf8c3075c7c10d98b7f1d4afcf2146',
-};
-
-// create LINE SDK client
-const client = new line.Client(config);
-
-// create Express app
-// about Express itself: https://expressjs.com/
-const app = express();
-
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
-app.post('/callback', line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
-    .catch((err) => {
-      console.error(err);
-      res.status(500).end();
-    });
+var bot = linebot({
+  channelId: '機器人',
+  channelSecret: '6baf8c3075c7c10d98b7f1d4afcf2146',
+  channelAccessToken: '295PO7c2FbLmq2jHznxmISTQciVcWy7DnEIArWCgSUKWPsorlKbbuRURYIYxWZYJXjlQ2Wj9Ik8nwuAz2qB2rc4EfKN56kdvBERkjx7F1OOPsjx8cdCNGhoLQtVfAR+TrFsuw73JsEKI2IA9gE719gdB04t89/1O/w1cDnyilFU='
 });
 
-// event handler
-function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
-    return Promise.resolve(null);
+
+
+//這邊想要做的是判讀如何回應
+bot.on('message', function(event) {
+  if (event.message.type = 'text') {
+    var msg = "https://tw.shop.com/search/"+event.message.text;
+  //收到文字訊息時，直接把收到的訊息傳回去
+    event.reply(msg).then(function(data) {
+      // 傳送訊息成功時，可在此寫程式碼 
+      console.log(msg);
+    }).catch(function(error) {
+      // 傳送訊息失敗時，可在此寫程式碼 
+      console.log('錯誤產生，錯誤碼：'+error);
+    });
   }
+});
 
-  // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
 
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
-}
-
-// listen on port
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`listening on ${port}`);
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log('目前的port是', port);
 });
