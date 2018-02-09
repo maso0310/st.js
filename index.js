5f0d9316b27dda81485b6a33845989de01b82b8f
unknown <balimon@hotmail.com>
2018-02-10 04:30:32 +0800

    'ok

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
-const request = require('request')
-const cheerio = require('cheerio')
+var linebot = require('linebot');
+var express = require('express');
 
-const url = 'http://www.cwb.gov.tw/V7/forecast/taiwan/Taipei_City.htm'
+var bot = linebot({
+  channelId: '機器人',
+  channelSecret: '6baf8c3075c7c10d98b7f1d4afcf2146',
+  channelAccessToken: '295PO7c2FbLmq2jHznxmISTQciVcWy7DnEIArWCgSUKWPsorlKbbuRURYIYxWZYJXjlQ2Wj9Ik8nwuAz2qB2rc4EfKN56kdvBERkjx7F1OOPsjx8cdCNGhoLQtVfAR+TrFsuw73JsEKI2IA9gE719gdB04t89/1O/w1cDnyilFU='
+});
 
-request(url, (err, res, body) => {
-  const $ = cheerio.load(body)
-  let weathers = []
-  $('#box8 .FcstBoxTable01 tbody tr').each(function(i, elem) {
-    weathers.push(
-      $(this)
-        .text()
-        .split('\n')
-    )
-  })
 
-  weathers = weathers.map(weather => ({
-    time: weather[1].substring(2).split(' ')[0],
-    temp: weather[2].substring(2),
-    rain: weather[6].substring(2),
-  }))
 
-  console.log(weathers)
-})
+//事件發生:用戶傳遞訊息，喚醒LINE BOT
+bot.on('message', function(event) {
+  //事件訊息屬性判定，是否為文字
+  if (event.message.type = 'text')  {
+    //設定要搜尋的網址頁面與用戶訊息之關聯
+    var msg = "https://tw.shop.com/search/"+event.message.text;
+    const request = require('request')
+    const cheerio = require('cheerio')
+    const url = msg
+    //跟網頁要資料，取得整個網頁"body"
+    request(url, (err, res, body) => {
+      console.log(body)
+    })
+    //把body放進cheerio準備分析
+    const $ = cheerio.load(body)
+    let output = []
+    $('#box8 .quickview-btn-box div img').each(function(i,elem){output.push($(this).image().split)('\n')})
+    console.log(output)
+  //收到文字訊息時，直接把收到的訊息傳回去
+    event.reply(output).then(function(data) {
+      // 傳送訊息成功時，可在此寫程式碼 
+      console.log(event.message.text);
+    }).catch(function(error) {
+      // 傳送訊息失敗時，可在此寫程式碼 
+      console.log('錯誤產生，錯誤碼：'+error);
+    });
+  }
+});
+
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
+
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log('目前的port是', port);
+});
