6edb5b3c8a6f9bbfe139877a04e330c6bcc856e2
unknown <balimon@hotmail.com>
2018-02-11 04:00:35 +0800

    'ok'

--- index.js
+++ index.js
@@ -16,8 +16,6 @@ bot.on('message', function(event) {
     var a ='我想找';
     var b =' ';
     
-    while (str.indexOf(a)=-1);
-      var msg = '請輸入"我想找"再打你想找的東西是什麼喔'
     while (str.indexOf(a)!=-1){ 
       str = str.replace(a,b);}
       var msg = "https://tw.shop.com/maso0310/search/"+str.trim();
@@ -30,6 +28,7 @@ bot.on('message', function(event) {
       console.log('錯誤產生，錯誤碼：'+error);
     });
   }
+  else ;break;
 });
 
 const app = express();
