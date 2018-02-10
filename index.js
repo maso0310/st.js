65a3acb2f5ba8b906c1ba7411ad8222f0b00d305
unknown <balimon@hotmail.com>
2018-02-11 02:47:08 +0800

    'ok'

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ var bot = linebot({
 //這邊想要做的是判讀如何回應
 bot.on('message', function(event) {
   var a = '我想找';
-  if (event.message.type = a +'text')  {
+  if (event.message.type ='text')  {
     url.indexOf(a)!=-1;
-    var msg = "https://tw.shop.com/maso0310/search/"+event.message.text;
+    var msg = "https://tw.shop.com/maso0310/search/"+event.message.text.replace(a);
       //收到文字訊息時，直接把收到的訊息傳回去
         event.reply(msg).then(function(data) {
       // 傳送訊息成功時，可在此寫程式碼 
