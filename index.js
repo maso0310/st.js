03b51c2c12e07a89eb38474fa80d05816352475a
unknown <balimon@hotmail.com>
2018-02-11 03:48:55 +0800

    'ok'

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ bot.on('message', function(event) {
     var str =event.message.text;
     var a ='我想找';
     var b =' ';
-    while (str.indexOf(a)!=-1) {
-      str = str.replace(a,b);}      
+    while (str.indexOf(a)!=-1);
+    ;continue; 
+    str = str.replace(a,b); 
     var msg = "https://tw.shop.com/maso0310/search/"+str.trim();
       //收到文字訊息時，直接把收到的訊息傳回去
         event.reply(msg).then(function(data) {
