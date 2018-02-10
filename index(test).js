4eb2308e84f3e550f78e19cecb1bf3e5fad806f3
unknown <balimon@hotmail.com>
2018-02-11 04:24:33 +0800

    'ok'

--- index(test).js
+++ index(test).js
@@ -7,31 +7,21 @@ var bot = linebot({
   channelAccessToken: '295PO7c2FbLmq2jHznxmISTQciVcWy7DnEIArWCgSUKWPsorlKbbuRURYIYxWZYJXjlQ2Wj9Ik8nwuAz2qB2rc4EfKN56kdvBERkjx7F1OOPsjx8cdCNGhoLQtVfAR+TrFsuw73JsEKI2IA9gE719gdB04t89/1O/w1cDnyilFU='
 });
 
-//事件發生:用戶傳遞訊息，喚醒LINE BOT
+
+
+//這邊想要做的是判讀如何回應
 bot.on('message', function(event) {
-  //事件訊息屬性判定，是否為文字
-  if (event.message.type = 'text')  {
-    //設定要搜尋的網址頁面與用戶訊息之關聯，還不知道怎麼做
-    //跟網頁要資料，取得整個網頁"body"
-    var request = require('request');
-    var url = "https://tw.shop.com/search/"+event.message.text;
-    request(url, function(error,res,body){
-      console.log(body);
-    });
-    //把body放進cheerio準備分析
-    var cheerio = require('cheerio');
-    var $ = cherrio.load(body)
-
-    var result = [];
-    $('section.search_resluts > ul#content > div.quickview-btn-box').each(function(){
-      result.push($(this).text().split('\n'));
-    });
-
-    
-  //收到文字訊息時，直接把收到的訊息傳回去
-    event.reply(result).then(function(data) {
+  if (event.message.type ='我想找'+'text')  {
+    var str =event.message.text;
+    var a ='我想找';
+    var b =' ';
+    while (str.indexOf(a)!=-1){ 
+    str = str.replace(a,b);}
+    var msg = "https://tw.shop.com/maso0310/search/"+str.trim();
+      //收到文字訊息時，直接把收到的訊息傳回去
+        event.reply(msg).then(function(data) {
       // 傳送訊息成功時，可在此寫程式碼 
-      console.log(event.message.text);
+      console.log(msg);
     }).catch(function(error) {
       // 傳送訊息失敗時，可在此寫程式碼 
       console.log('錯誤產生，錯誤碼：'+error);
--- index.js
+++ index.js
@@ -11,15 +11,10 @@ var bot = linebot({
 
 //這邊想要做的是判讀如何回應
 bot.on('message', function(event) {
-  if (event.message.type ='我想找'+'text')  {
-    var str =event.message.text;
-    var a ='我想找';
-    var b =' ';
-    while (str.indexOf(a)!=-1){ 
-    str = str.replace(a,b);}
-    var msg = "https://tw.shop.com/maso0310/search/"+str.trim();
-      //收到文字訊息時，直接把收到的訊息傳回去
-        event.reply(msg).then(function(data) {
+  if (event.message.type = 'text') {
+    var msg = "https://tw.shop.com/maso0310/search/"+event.message.text;
+  //收到文字訊息時，直接把收到的訊息傳回去
+    event.reply(msg).then(function(data) {
       // 傳送訊息成功時，可在此寫程式碼 
       console.log(msg);
     }).catch(function(error) {
