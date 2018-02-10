e58bc2d214fe3cf93738fc2f8fd6d2b6f9147060
unknown <balimon@hotmail.com>
2018-02-11 03:01:13 +0800

    'ok'

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ bot.on('message', function(event) {
     url.indexOf(a)!=-1;
     then 
     var str = url.replace(a);
-      var msg = "https://tw.shop.com/maso0310/search/"+event.message.text.replace(a);
+    var msg = "https://tw.shop.com/maso0310/search/"+ str ;
       //收到文字訊息時，直接把收到的訊息傳回去
         event.reply(msg2).then(function(data) {
       // 傳送訊息成功時，可在此寫程式碼 
