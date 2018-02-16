8c2eba322ebd2b2c40d92816a82e2f2013a85355
unknown <balimon@hotmail.com>
2018-02-17 01:17:35 +0800

    'ok'

--- index.js
+++ index.js
@@ -13,10 +13,18 @@ var bot = linebot({
 bot.on('message', function(event) {
   if (event.message.type = 'text') {
     var msg = "https://tw.shop.com/maso0310/search/"+event.message.text;
+    var request = require('request');
+    var cheerio = require('cheerio');
+    var $ = cheerio.load(body);
+    var shop = [];
+    request({msg, headers:{'user-agent':'node.js'}}),function(err,res,body)
+    {
+    $ = cheerio.load(body);$('div p').each(function(i,elem){shop.push($(this).text())})
+    }
   //收到文字訊息時，直接把收到的訊息傳回去
-    event.reply(msg).then(function(data) {
+    event.reply(shop).then(function(data) {
       // 傳送訊息成功時，可在此寫程式碼 
-      console.log(msg);
+      console.log(shop);
     }).catch(function(error) {
       // 傳送訊息失敗時，可在此寫程式碼 
       console.log('錯誤產生，錯誤碼：'+error);
