254475863101cd216024b8b51f7e7bc49e2cfc0f
unknown <balimon@hotmail.com>
2018-02-17 00:47:46 +0800

    'ok'

--- index.js
+++ index.js
@@ -7,31 +7,34 @@ var bot = linebot({
   channelAccessToken: '295PO7c2FbLmq2jHznxmISTQciVcWy7DnEIArWCgSUKWPsorlKbbuRURYIYxWZYJXjlQ2Wj9Ik8nwuAz2qB2rc4EfKN56kdvBERkjx7F1OOPsjx8cdCNGhoLQtVfAR+TrFsuw73JsEKI2IA9gE719gdB04t89/1O/w1cDnyilFU='
 });
 
-
-
-//這邊想要做的是判讀如何回應
+//事件發生:用戶傳遞訊息，喚醒LINE BOT
 bot.on('message', function(event) {
-  if (event.message.type = 'text') {
+  //事件訊息屬性判定，是否為文字
+  if (event.message.type = 'text')  {
+    //設定要搜尋的網址頁面與用戶訊息之關聯，還不知道怎麼做
+    //跟網頁要資料，取得整個網頁"body"
     var request = require('request')
     var url = 'http://tw.shop.com/maso0310/search/htc'
     request({url ,headers:{'user-agent':'node.js'} },
-      function (err, res, body){
-      console.log(body)
-      const cheerio = require('cheerio');
-    // 把 body 放進 cheerio 準備分析
-      const $ = cheerio.load(body);
-    let shop = []
-    $(' article.product clear ')
-    .each(function(i, elem) {
-      shop.push($(this).image())
-      event.reply(shop).then(function(data) {
-        // 傳送訊息成功時，可在此寫程式碼 
-        console.log(shop);
-      }).catch(function(error) {
-        // 傳送訊息失敗時，可在此寫程式碼 
-        console.log('錯誤產生，錯誤碼：'+error);
-      });
-    })
+      function (err, res, body)
+      {
+        const cheerio = require('cheerio');
+        const $ = cheerio.load(body);
+        let shop = []
+        $('li.clear a')
+        .each(function(i, elem) {
+        shop.push($(this).text())
+
+        event.reply(shop).then(function(data) {
+          // 傳送訊息成功時，可在此寫程式碼 
+          console.log(shop);
+        }).catch(function(error) {
+          // 傳送訊息失敗時，可在此寫程式碼 
+          console.log('錯誤產生，錯誤碼：'+error);
+        });
+      })
+      })
+  }
 });
 
 const app = express();
@@ -41,4 +44,4 @@ app.post('/', linebotParser);
 var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
   console.log('目前的port是', port);
-})
+});
--- request3.js
+++ request3.js
@@ -1,17 +1,14 @@
 var request = require('request')
 var url = 'http://tw.shop.com/maso0310/search/htc'
 request({url ,headers:{'user-agent':'node.js'} },
-  function (err, res, body){
-  //console.log(body)
-
-
-const cheerio = require('cheerio');
-// 把 body 放進 cheerio 準備分析
-const $ = cheerio.load(body);
-let shop = []
-$(' article.product clear ')
-.each(function(i, elem) {
-  shop.push($(this).image())
+  function (err, res, body)
+  {
+  const cheerio = require('cheerio');
+  const $ = cheerio.load(body);
+  let shop = []
+  $('li.clear a')
+  .each(function(i, elem) {
+  shop.push($(this).text())
   console.log(shop)
 })
 })
