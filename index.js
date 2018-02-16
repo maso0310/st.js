b8bb54881a235dc7445dc37b528fe65dc0be6730
unknown <balimon@hotmail.com>
2018-02-17 01:10:54 +0800

    'ok'

--- index.js
+++ index.js
@@ -7,31 +7,20 @@ var bot = linebot({
   channelAccessToken: '295PO7c2FbLmq2jHznxmISTQciVcWy7DnEIArWCgSUKWPsorlKbbuRURYIYxWZYJXjlQ2Wj9Ik8nwuAz2qB2rc4EfKN56kdvBERkjx7F1OOPsjx8cdCNGhoLQtVfAR+TrFsuw73JsEKI2IA9gE719gdB04t89/1O/w1cDnyilFU='
 });
 
-//事件發生:用戶傳遞訊息，喚醒LINE BOT
+
+
+//這邊想要做的是判讀如何回應
 bot.on('message', function(event) {
-  //事件訊息屬性判定，是否為文字
-  if (event.message.type = 'text')  
-  {
-    var request = require('request')
-    var url = 'http://tw.shop.com/maso0310/search/'+event.message.text;
-    request({url ,headers:{'user-agent':'node.js'} },
-      function (err, res, body)
-      { console.log(body)
-        const cheerio = require('cheerio');
-        const $ = cheerio.load(body);
-        let shop = [];
-        $('div.product-info')
-        .each(function(i, elem) {
-        shop.push($(this).text())
-      })
-      })
-      event.reply(shop).then(function(data) {
-        // 傳送訊息成功時，可在此寫程式碼 
-        console.log(shop);
-      }).catch(function(error) {
-        // 傳送訊息失敗時，可在此寫程式碼 
-        console.log('錯誤產生，錯誤碼：'+error);
-      });
+  if (event.message.type = 'text') {
+    var msg = "https://tw.shop.com/maso0310/search/"+event.message.text;
+  //收到文字訊息時，直接把收到的訊息傳回去
+    event.reply(msg).then(function(data) {
+      // 傳送訊息成功時，可在此寫程式碼 
+      console.log(msg);
+    }).catch(function(error) {
+      // 傳送訊息失敗時，可在此寫程式碼 
+      console.log('錯誤產生，錯誤碼：'+error);
+    });
   }
 });
 
