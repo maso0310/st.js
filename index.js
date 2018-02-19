cdf661707dca210f80b72a8d0032a377609d13ad
unknown <balimon@hotmail.com>
2018-02-20 04:18:42 +0800

    'ok'

--- index.js
+++ index.js
@@ -1,33 +1,5 @@
-const express = require('express')
-const middleware = require('@line/bot-sdk').middleware
-const JSONParseError = require('@line/bot-sdk').JSONParseError
-const SignatureValidationFailed = require('@line/bot-sdk').SignatureValidationFailed
-
-const app = express()
-
-const config = {
-  channelAccessToken: '295PO7c2FbLmq2jHznxmISTQciVcWy7DnEIArWCgSUKWPsorlKbbuRURYIYxWZYJXjlQ2Wj9Ik8nwuAz2qB2rc4EfKN56kdvBERkjx7F1OOPsjx8cdCNGhoLQtVfAR+TrFsuw73JsEKI2IA9gE719gdB04t89/1O/w1cDnyilFU=',
-  channelSecret: '6baf8c3075c7c10d98b7f1d4afcf2146'
-}
-
-app.use(middleware(config))
-
-app.post('/webhook', (req, res) => {
-  res.json(req.body.events) // req.body will be webhook event object
-})
-
-app.use((err, req, res, next) => {
-  if (err instanceof SignatureValidationFailed) {
-    res.status(401).send(err.signature)
-    return
-  } else if (err instanceof JSONParseError) {
-    res.status(400).send(err.raw)
-    return
-  }
-  next(err) // will throw default 500
-})
-
-app.listen(8080)
+var linebot = require('linebot');
+var express = require('express');
 
 var bot = linebot({
   channelId: '機器人',
@@ -42,16 +14,7 @@ bot.on('message', function(event) {
   if (event.message.type = 'text') {
     var msg = "https://tw.shop.com/maso0310/search/"+event.message.text;
     var request = require('request')
-    request({url ,headers:{'X-Line-Signature':''} },
-    function (err, res, body)
-    {
-    const cheerio = require('cheerio');
-    const $ = cheerio.load(body);
-    const $imgdom = $("div.product-image");
-    $imgdom.find('img').each(function(index , el) {
-    var imgurl = $(this).attr("src")    
-    })
-    })
+
   //收到文字訊息時，直接把收到的訊息傳回去
     event.reply({
       type: 'template',
