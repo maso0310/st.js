818357f78b02085d56d94e3c651c62bb8d9f284a
unknown <balimon@hotmail.com>
2018-02-20 01:33:36 +0800

    'ok'

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ bot.on('message', function(event) {
   //收到文字訊息時，直接把收到的訊息傳回去
     event.reply({
       type: 'imagemap',
-      baseUrl: "https://tw.shop.com/maso0310/search/"+event.message.text+"?_ignore=",
+      baseUrl: "https://tw.shop.com/search/HTC?_ignore=",
       altText: 'this is an imagemap',
       baseSize: { height: 1040, width: 1040 },
       actions: [{
         type: 'uri',
-        linkUri: "https://tw.shop.com/maso0310/search/"+event.message.text+"1040",
+        linkUri: "https://tw.shop.com/search/HTC/1040",
         area: { x: 0, y: 0, width: 520, height: 1040 }
       }, {
         type: 'message',
