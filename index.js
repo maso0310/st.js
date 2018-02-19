b37388c2ad3dd797946bc294b0e5b98d649fae84
unknown <balimon@hotmail.com>
2018-02-20 03:15:55 +0800

    'ok'

--- index.js
+++ index.js
@@ -14,16 +14,7 @@ bot.on('message', function(event) {
   if (event.message.type = 'text') {
   var request = require('request')
   var url = "https://tw.shop.com/maso0310/search/"+event.message.text;
-  request({url ,headers:{'user-agent':'node.js'} },
-  function (err, res, body)
-  {
-  const cheerio = require('cheerio');
-  const $ = cheerio.load(body);
-  const $imgdom = $("div.product-image");
-  $imgdom.find('img').each(function(index , el) {
-  var imgurl = $(this).attr("src");
-  })
-  })
+
 
   //收到文字訊息時，直接把收到的訊息傳回去
     event.reply({
