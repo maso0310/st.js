1784f92cf4ad22f6554c1a34f28203119c726ef5
unknown <balimon@hotmail.com>
2018-02-20 01:21:44 +0800

    'ok'

--- index.js
+++ index.js
@@ -6,56 +6,43 @@ var bot = linebot({
   channelSecret: '6baf8c3075c7c10d98b7f1d4afcf2146',
   channelAccessToken: '295PO7c2FbLmq2jHznxmISTQciVcWy7DnEIArWCgSUKWPsorlKbbuRURYIYxWZYJXjlQ2Wj9Ik8nwuAz2qB2rc4EfKN56kdvBERkjx7F1OOPsjx8cdCNGhoLQtVfAR+TrFsuw73JsEKI2IA9gE719gdB04t89/1O/w1cDnyilFU='
 });
-  // 使用 Request Promise 發送自定義的訊息
-const request = require('request-promise');
 
-// Line Bot 的 Channel Token
-const lineBotToken = '...';
 
-// 收到 User Request 時要發回給 User 的訊息放這裡
-router.post('/webhooks', function *(ctx,  next) {
-  // User 給 Bot 的訊息是一個 Array
-  const userMessages = ctx.request.body.events;
-  
-  // 準備回覆給 User 的訊息
-  let replyMessages = [];
-  replyMessages = userMessages.map((currentReplyMessages, userMessage) => {
-    // 等下放我們要回給 User 的訊息，一次最多 5 則
-    let messages;
 
-    // User 傳來的訊息型態
-    if ( userMessage.type === 'message' ) {
-      messages = [
-        {type: 'text', text: '你成功了！我回你就是了'},
-        {type: 'image', originalContentUrl: 'http://i.imgur.com/xjTET8v.jpg', previewImageUrl: 'https://sdl-stickershop.line.naver.jp/stickershop/v1/product/1179553/iphone/main@2x.png'},
-      ];
-      
-    }
-    else {
-      messages = [{type: 'text', text: '收到囉！'}];
-    }
-    
-    // request 的 options
-    let options = {
-        method: 'POST',
-        uri: 'https://api.line.me/v2/bot/message/reply',
-        // 客制化 headers 內容，主要告訴 Line 收到的訊息格式和 Line Bot 的 Token
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${lineBotToken}`,
-        },
-        body: {
-          // 告訴 Line 這個訊息回覆給誰
-          replyToken: userMessage.replyToken,
-          messages: messages,
-        },
-        json: true,
-      };
+//這邊想要做的是判讀如何回應
+bot.on('message', function(event) {
+  if (event.message.type = 'text') {
+    var msg = "https://tw.shop.com/maso0310/search/"+event.message.text;
+  //收到文字訊息時，直接把收到的訊息傳回去
+    event.reply({
+      type: 'imagemap',
+      baseUrl: "https://img.shop.com/Image/250000/252100/252124/products/1571889306__175x175__.jpg",
+      altText: 'this is an imagemap',
+      baseSize: { height: 175, width: 175 },
+      actions: [{
+        type: 'uri',
+        linkUri: "https://img.shop.com/Image/250000/252100/252124/products/1571889306__175x175__.jpg",
+        area: { x: 500, y: 500, width: 175, height: 175 }
+      }, {
+        type: 'message',
+        text: 'hello',
+        area: { x: 500, y: 500, width: 175, height: 175 }
+      }]
+    }).then(function(data) {
+      // 傳送訊息成功時，可在此寫程式碼 
+      console.log(msg);
+    }).catch(function(error) {
+      // 傳送訊息失敗時，可在此寫程式碼 
+      console.log('錯誤產生，錯誤碼：'+error);
+    });
+  }
+});
 
-    return request(options);
-  });
-  
-  // 訊息處理好就等待所有 Request 發送出去
-  yield Promise.all( replyMessages );
-  
-})
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
+
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log('目前的port是', port);
+});
